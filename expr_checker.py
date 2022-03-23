06d24d3a702f8b213969223fcf9152e543cb4130
Keyboard Slayer <40356204+keyboard-slayer@users.noreply.github.com>
2022-03-23 14:39:25 +0100

    [FEAT] add ast_get_attr

--- expr_checker.py
+++ expr_checker.py
@@ -2,18 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import ast
-from copy import copy
 from inspect import getsource
 from textwrap import dedent
+
 
 def ast_default_check_type(method, value):
     safe_type = (str, int, type(None), type(range(0)))
 
-    if isinstance(value, (list, tuple, set)):
-        for val in value:
-            ast_default_check_type(method, val)
-        return value
-    elif type(value) not in safe_type:
+    if hasattr(value, "__self__") and type(value.__self__) not in safe_type and type(value) not in safe_type:
         raise ValueError(f"safe_eval didn't like {value}")
 
     return value
@@ -22,7 +18,7 @@ def ast_default_check_type(method, value):
 def ast_default_test(func, check_type, *args, **kwargs):
     for arg in args:
         check_type("arguments", arg)
-    
+
     for arg in kwargs.values():
         check_type("arguments", arg)
 
@@ -33,10 +29,9 @@ def ast_default_test(func, check_type, *args, **kwargs):
 
 
 class NodeChecker(ast.NodeTransformer):
-    def __init__(self, check_fn, allow_function_calls, check_type_fn, allowed_attr, get_attr):
+    def __init__(self, check_fn, allow_function_calls, check_type_fn, get_attr):
         self.check_fn = check_fn.__name__
         self.check_type_fn = check_type_fn.__name__
-        self.allowed_attr = allowed_attr
         self.fncall = allow_function_calls
         self.getattr = get_attr.__name__
 
@@ -45,21 +40,17 @@ class NodeChecker(ast.NodeTransformer):
         super().__init__()
 
     def visit_Call(self, node):
-        list(map(self.visit, node.args))
+        self.generic_visit(node)
 
         if not self.fncall:
-            raise Exception("safe_eval didn't allow you to call any functions")
+            raise Exception("safe_eval didn't permit you to call any functions")
 
         if isinstance(node.func, ast.Attribute):
-            if node.func.attr not in self.allowed_attr:
-                raise ValueError(
-                    f"safe_eval doesn't allow you to read {node.func.attr}")
-
-            name = copy(node.func.value)
-            node.func.value.__class__ = ast.Call
-            node.func.value.func = ast.Name(self.getattr, ctx=ast.Load())
-            node.func.value.args = [name]
-            node.func.value.keywords = []
+            return ast.Call(
+                func=ast.Name(self.getattr, ast.Load()),
+                args=[node.func.value, ast.Constant(node.func.attr), node],
+                keywords=[]
+            )
 
         else:
             self.generic_visit(node.func)
@@ -77,49 +68,49 @@ class NodeChecker(ast.NodeTransformer):
 
         return node
 
-    def visit_Name(self, node):
-        if node.id in self.reserved_name:
-            raise NameError(f"safe_eval: {node.id} is a reserved name")
-
-        return node
-
     def visit_Attribute(self, node):
-        if node.attr not in self.allowed_attr:
-            raise ValueError(
-                f"safe_eval doesn't allow you to read {node.attr}")
-
         if isinstance(node.ctx, ast.Load):
+            subcall = ast.Call(
+                func=ast.Name(self.getattr, ctx=ast.Load()),
+                args=[node.value, ast.Constant(node.attr), node],
+                keywords=[]
+            )
+
             return ast.Call(
                 func=ast.Name(self.check_type_fn, ctx=ast.Load()),
-                args=[ast.Constant("attribute"), node],
+                args=[ast.Constant("attribute"), subcall],
                 keywords=[]
             )
 
         elif isinstance(node.ctx, ast.Store):
-            return ast.Call(
-                func=ast.Name(self.getattr, ctx=ast.Load()),
-                args=[node.value],
-                keywords=[]
-            )
+            raise ValueError("safe_eval: doesn't permit you to store values in attributes")
 
         elif isinstance(node.ctx, ast.Del):
-            raise ValueError("safe_eval: you can't delete attributes")
+            raise ValueError("safe_eval: doesn't permit you to delete attributes")
 
     def visit_Assign(self, node):
-        list(map(self.visit, node.targets))
+        self.generic_visit(node.value)
 
-        node.value = ast.Call(
-            func=ast.Name(self.check_type_fn, ctx=ast.Load()),
-            args=[ast.Constant("assignation"), node.value],
-            keywords=[]
-        )
+        for target in node.targets:
+            if isinstance(target, ast.Name):
+                if target.id in self.reserved_name:
+                    raise NameError(f"safe_eval: {target.id} is a reserved name")
+            elif isinstance(target, ast.Attribute):
+                raise ValueError("safe_eval: doesn't permit you to modify an attribute")  # FIXME with ast_set_attr
+            elif isinstance(target, ast.Subscript) and isinstance(target.value,
+                                                                  ast.Call) and target.value.func.id in ["globals",
+                                                                                                         "locals"]:
+                raise ValueError("safe_eval: doesn't permit you to modify locals() and globals()")
+            else:
+                raise NotImplementedError(f"{ast.dump(target, indent=4)}")
 
         return node
 
 
-def expr_checker(expr, allowed_attr, get_attr, allow_function_calls=True, check_type=ast_default_check_type, check_function=ast_default_test):
+def expr_checker(expr, get_attr, allow_function_calls=True, check_type=ast_default_check_type,
+                 check_function=ast_default_test):
     code = f"""{dedent(getsource(check_type))}
 {dedent(getsource(check_function))}
-{ast.unparse(NodeChecker(check_function, allow_function_calls, check_type, allowed_attr, get_attr).visit(ast.parse(expr)))}
+{ast.unparse(NodeChecker(check_function, allow_function_calls, check_type, get_attr).visit(ast.parse(expr)))}
     """
     return code
--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 
 import unittest
 from inspect import cleandoc
+
 from expr_checker import *
 
 
@@ -13,11 +14,18 @@ class Dangerous:
 class Good:
     a = "hi"
     evil = Dangerous()
+    eevil = 4
 
     _secret_stuff = "1+1=3"
 
     def tell_me_hi(self):
         return self.a
+
+    def gift_of_satan(self):
+        return 666
+
+    def gather_secret(self):
+        return self._secret_stuff
 
     def __enter__(self):
         return Good()
@@ -35,94 +43,77 @@ class ReadOnlyObject:
 
 
 def check_type(method, value):
-    safe_type = (str, int, type(None), type(range(0)), Good, type(lambda x:x))
+    safe_type = (str, int, type(None), type(range(0)), Good, type(lambda x: x), ReadOnlyObject)
 
-    if type(value) in (list, tuple, set):
-        for val in value:
-            check_type(method, val)
-        return value
-    elif type(value) == dict:
-        for key in value.keys():
-            check_type(method, key)
-        for val in value.values():
-            check_type(method, val)
-        return value
-    elif type(value) not in safe_type:
+    if type(value) not in safe_type and not (hasattr(value, "__self__") and type(value.__self__) in safe_type):
         raise ValueError(f"safe_eval didn't like {value}")
     else:
         return value
 
-def ast_get_attr(obj):
-    if type(obj) in {ReadOnlyObject}:
-        return copy(obj)
-    else:
-        return obj
 
-_ALLOWED_ATTR = {"a", "x", "tell_me_hi", "set_x_value"}
+def ast_get_attr(obj, key, value):
+    if key not in {"a", "x", "tell_me_hi", "set_x_value"}:
+        raise ValueError(f"safe_eval doesn't permit you to read {key}")
+
+    return value
+
 
 class TestFuncChecker(unittest.TestCase):
     def test_function_call(self):
         def abc(a, b, c):
             return f"A: {a}, B: {b}, C: {c}"
 
-        eval(compile(expr_checker(
-            "abc('aaa', 'bbb', 'ccc')", _ALLOWED_ATTR, ast_get_attr), "", "exec"))
+        eval(compile(expr_checker("abc('aaa', 'bbb', 'ccc')", ast_get_attr), "", "exec"))
 
         with self.assertRaisesRegex(ValueError, "safe_eval didn't like <__main__.Dangerous object at .+>"):
-            exec(expr_checker("abc(a='aaa', c='ccc', b=Dangerous())",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-
+            exec(expr_checker("abc(a='aaa', c='ccc', b=Dangerous())", ast_get_attr, check_type=check_type))
 
     def test_comp_expr(self):
-        exec(expr_checker(
-            "[(lambda x: x**2)(n) for n in range(1, 11)]", _ALLOWED_ATTR, ast_get_attr, check_type=check_type)),
+        exec(expr_checker("[(lambda x: x**2)(n) for n in range(1, 11)]", ast_get_attr, check_type=check_type)),
 
     def test_attribute(self):
         a = Good()
-        exec(expr_checker("a.a", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-        exec(expr_checker("a.tell_me_hi()", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        exec(expr_checker("a.a", ast_get_attr, check_type=check_type))
+        exec(expr_checker("a.tell_me_hi()", ast_get_attr, check_type=check_type))
 
-        with self.assertRaisesRegex(ValueError, "safe_eval doesn't allow you to read evil"):
-            exec(expr_checker("a.evil", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        with self.assertRaisesRegex(ValueError, "safe_eval doesn't permit you to read eevil"):
+            exec(expr_checker("a.eevil", ast_get_attr, check_type=check_type))
 
-        with self.assertRaisesRegex(ValueError, "safe_eval doesn't allow you to read gather_secret"):
-            exec(expr_checker("a.gather_secret()",
-                _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        with self.assertRaisesRegex(ValueError, "safe_eval doesn't permit you to read gather_secret"):
+            exec(expr_checker("a.gather_secret()", ast_get_attr, check_type=check_type))
 
-    def test_reaonly_type(self):
-        a = ReadOnlyObject(42)
-
-        exec(expr_checker("a.set_x_value(25)",
-            _ALLOWED_ATTR, ast_get_attr))
-
-        self.assertEqual(a.x, 42)
+    # FIXME: Need to implement ast_set_attr
+    # def test_readonly_type(self):
+    #     a = ReadOnlyObject(42)
+    #
+    #     exec(expr_checker("a.set_x_value(25)", ast_get_attr, check_type=check_type))
+    #
+    #     self.assertEqual(a.x, 42)
 
     def test_delete_attr(self):
-        with self.assertRaisesRegex(ValueError, "safe_eval: you can't delete attributes"):
-            exec(expr_checker("del a.x", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        with self.assertRaisesRegex(ValueError, "safe_eval: doesn't permit you to delete attributes"):
+            exec(expr_checker("del a.x", ast_get_attr, check_type=check_type))
 
     def test_method_return(self):
-        exec(expr_checker("Good().tell_me_hi()",
-            _ALLOWED_ATTR, ast_get_attr, check_type=check_type)),
+        exec(expr_checker("Good().tell_me_hi()", ast_get_attr, check_type=check_type)),
 
-        with self.assertRaisesRegex(ValueError, "safe_eval doesn't allow you to read gift_of_satan"):
-            exec(expr_checker("Good().gift_of_satan()",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        with self.assertRaisesRegex(ValueError, "safe_eval doesn't permit you to read gift_of_satan"):
+            exec(expr_checker("Good().gift_of_satan()", ast_get_attr, check_type=check_type))
 
     def test_object_with(self):
         exec(expr_checker(cleandoc("""
             with Good() as g:
-                g.tell_me_hi()"""), _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+                g.tell_me_hi()"""), ast_get_attr, check_type=check_type))
 
-        with self.assertRaisesRegex(ValueError, "safe_eval doesn't allow you to read gift_of_satan"):
+        with self.assertRaisesRegex(ValueError, "safe_eval doesn't permit you to read gift_of_satan"):
             exec(expr_checker(cleandoc("""
                 with Good() as g:
-                    g.gift_of_satan()"""), _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+                    g.gift_of_satan()"""), ast_get_attr, check_type=check_type))
 
         with self.assertRaisesRegex(ValueError, "safe_eval didn't like <__main__.Dangerous object at .+>"):
             exec(expr_checker(cleandoc("""
                 with Dangerous() as d:
-                    pass"""), _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+                    pass"""), ast_get_attr, check_type=check_type))
 
     def test_function_return(self):
         def foo():
@@ -131,10 +122,10 @@ class TestFuncChecker(unittest.TestCase):
         def anti_foo():
             return Dangerous()
 
-        exec(expr_checker("foo()", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        exec(expr_checker("foo()", ast_get_attr, check_type=check_type))
 
         with self.assertRaisesRegex(ValueError, "safe_eval didn't like <__main__.Dangerous object at .+>"):
-            exec(expr_checker("anti_foo()", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            exec(expr_checker("anti_foo()", ast_get_attr, check_type=check_type))
 
     def test_evil_decorator(self):
         def evil(func):
@@ -145,43 +136,39 @@ class TestFuncChecker(unittest.TestCase):
             pass
 
         with self.assertRaisesRegex(ValueError, "safe_eval didn't like <__main__.Dangerous object at .+>"):
-            exec(expr_checker("foo()", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            code = expr_checker("foo()", ast_get_attr, check_type=check_type)
+            exec(code)
+            print(code)
 
     def test_evil_exec(self):
         with self.assertRaisesRegex(ValueError, "safe_eval didn't like 3.1415"):
-            exec(expr_checker("eval('3.1415')",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            exec(expr_checker("eval('3.1415')", ast_get_attr, check_type=check_type))
 
     def test_from_good_to_dangerous(self):
         a = Good()
 
-        with self.assertRaisesRegex(ValueError, "safe_eval doesn't allow you to read __class__"):
-            exec(expr_checker("a.__class__ = Dangerous",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        with self.assertRaisesRegex(ValueError, "safe_eval: doesn't permit you to modify an attribute"):
+            exec(expr_checker("a.__class__ = Dangerous", ast_get_attr, check_type=check_type))
 
     def test_hijacking_global(self):
-        with self.assertRaisesRegex(ValueError, "safe_eval didn't like <class '__main__.Dangerous'>"):
-            exec(expr_checker(
-                "globals()['Good'] = Dangerous", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        with self.assertRaises(ValueError):
+            exec(expr_checker("globals()['Good'] = Dangerous", ast_get_attr, check_type=check_type))
 
-        with self.assertRaisesRegex(ValueError, "safe_eval didn't like .+"): # It didn't like something in the dict of globals()
-            exec(expr_checker(cleandoc( "globals()['evil'] = lambda: Dangerous()"), _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        with self.assertRaises(ValueError):
+            exec(expr_checker(cleandoc("locals()['evil'] = lambda: Dangerous()"), ast_get_attr, check_type=check_type))
 
     def test_forbidden_attr(self):
         a = Good()
 
         with self.assertRaises(ValueError):
-            exec(expr_checker("print(a._secret_stuff)",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            exec(expr_checker("print(a._secret_stuff)", ast_get_attr, check_type=check_type))
 
         with self.assertRaises(ValueError):
-            exec(expr_checker("a._secret_stuff = 42",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            exec(expr_checker("a._secret_stuff = 42", ast_get_attr, check_type=check_type))
 
     def test_dangerous_lambda(self):
         with self.assertRaises(ValueError):
-            exec(expr_checker("(lambda: print(Dangerous()))()",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            exec(expr_checker("(lambda: print(Dangerous()))()", ast_get_attr, check_type=check_type))
 
     def test_multiline(self):
         code = cleandoc("""
@@ -191,15 +178,14 @@ class TestFuncChecker(unittest.TestCase):
             b()
         """)
 
-        exec(expr_checker(code, _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+        exec(expr_checker(code, ast_get_attr, check_type=check_type))
 
     def test_file_open(self):
         a = Good()
         a.evil = open("test.py")
 
         with self.assertRaises(ValueError):
-            exec(expr_checker("print(a.evil)",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            exec(expr_checker("print(a.evil)", ast_get_attr, check_type=check_type))
 
         a.evil.close()
 
@@ -216,35 +202,11 @@ class TestFuncChecker(unittest.TestCase):
                 check_type = lambda t: t
                 c = a.evil""")
 
-        _ALLOWED_ATTR.add("evil")
+        with self.assertRaises(NameError):
+            exec(expr_checker(code, ast_get_attr, check_type=check_type))
 
         with self.assertRaises(NameError):
-            exec(expr_checker(code, _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-
-        with self.assertRaises(NameError):
-            exec(expr_checker(code2, _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-
-    def test_collections(self):
-        def a(a):
-            return a
-
-        exec(expr_checker("a([1, Good(), 3])",
-             _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-        exec(expr_checker("a((4, 5, 6))", _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-        exec(expr_checker("a({7, 'hi', None})",
-             _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-
-        with self.assertRaises(ValueError):
-            exec(expr_checker("a([1.5, 2, 3])",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-
-        with self.assertRaises(ValueError):
-            exec(expr_checker("a((4, Dangerous(), 'lol'))",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
-
-        with self.assertRaises(ValueError):
-            exec(expr_checker("a({4, 3.1415, 3})",
-                 _ALLOWED_ATTR, ast_get_attr, check_type=check_type))
+            exec(expr_checker(code2, ast_get_attr, check_type=check_type))
 
     def test_isinstance_bad_idea(self):
         class Dangerous2(Good):
@@ -265,22 +227,23 @@ class TestFuncChecker(unittest.TestCase):
         code = "(1, 'Hi', Dangerous2())"
 
         # This pass
-        exec(expr_checker(code, _ALLOWED_ATTR, ast_get_attr, check_type=check_type2))
+        exec(expr_checker(code, ast_get_attr, check_type=check_type2))
 
         with self.assertRaises(ValueError):
             # This doesn't 
-           exec(expr_checker(code, _ALLOWED_ATTR, ast_get_attr, check_type=check_type)) 
+            exec(expr_checker(code, ast_get_attr, check_type=check_type))
 
     def test_deny_function_call(self):
         with self.assertRaises(Exception) as e:
-            exec(expr_checker("print('Hello, World')", _ALLOWED_ATTR, ast_get_attr, allow_function_calls=False))
+            exec(expr_checker("print('Hello, World')", ast_get_attr, allow_function_calls=False))
 
-        self.assertEqual(e.exception.args[0], "safe_eval didn't allow you to call any functions")
+        self.assertEqual(e.exception.args[0], "safe_eval didn't permit you to call any functions")
 
         with self.assertRaises(Exception) as e:
-            exec(expr_checker("kanban.get('sold')", _ALLOWED_ATTR, ast_get_attr, allow_function_calls=False))
+            exec(expr_checker("kanban.get('sold')", ast_get_attr, allow_function_calls=False))
 
-        self.assertEqual(e.exception.args[0], "safe_eval didn't allow you to call any functions")
+        self.assertEqual(e.exception.args[0], "safe_eval didn't permit you to call any functions")
+
 
 if __name__ == "__main__":
     unittest.main()
